7a9b6511e5cbd8fdba66190e6f8cceab6e1f11ee
Code_yks <48611034+yksCode@users.noreply.github.com>
2023-05-15 17:41:16 +0800

    Update TEST.java

--- src/com/dataAuditLog/TEST.java
+++ src/com/dataAuditLog/TEST.java
@@ -8,13 +8,16 @@ public class TEST {
         //插入一条用户访问记录
         try {
             logSgn.sign("{action:insert}\n" +
-                            "{data:{hello world}" ,
+                            "{data:{hello world}\n" +
+                            "{time:2023-5-13 17:31}",
                     "yks");
             logSgn.sign("{action:insert}\n" +
-                            "{data:{hello world}" ,
+                            "{data:{hello world}\n" +
+                            "{time:2023-5-13 17:31}",
                     "yks");
             logSgn.sign("{action:insert}\n" +
-                            "{data:{hello world}" ,
+                            "{data:{hello world}\n" +
+                            "{time:2023-5-13 17:31}",
                     "yks");
             logSgn.getHchain().getUHC("yks").printInfo();
         } catch (Exception e) {
