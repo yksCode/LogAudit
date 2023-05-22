0de5d276fc97ca08820d0d35fb534245b89f2283
Code_yks <48611034+yksCode@users.noreply.github.com>
2023-05-22 17:15:18 +0800

    Update userHashChain.java

--- src/com/dataAuditLog/userHashChain.java
+++ src/com/dataAuditLog/userHashChain.java
@@ -10,26 +10,63 @@ public class userHashChain {
 
     private String Id;
     private String userName;
-    private List<byte[]> hashChain;//存储用户
+    private List<String> hashChain;//存储用户
+    private List<String> logChain;//存储log条目
     public userHashChain(String id) {
         this.Id = id;
-//        this.userName = userName;
         this.hashChain = new ArrayList<>();
+        this.logChain  = new ArrayList<>();
     }
 
-    public void setHashChain(byte[] digitalSignature) {
+    public void setHashChain(String digitalSignature) {
         hashChain.add(digitalSignature);
     }
 
-    public List<byte[]> getHashChain() {
+    public void setLogChain(String log){
+        logChain.add(log);
+    }
+    public List<String> getHashChain() {
         return hashChain;
+    }
+    public int hashChainLen(){
+        return hashChain.size();
+    }
+    public String getLastSgn(){
+        if(hashChainLen() != 0)
+            return hashChain.get(hashChainLen()-1);
+        else
+            return null;
     }
 
     public void printInfo(){
         System.out.println("ID:"+this.Id);
         System.out.println("Signature Chain:");
-        for(byte[] str:this.hashChain){
+        for(String str:this.hashChain){
             System.out.println(str);
         }
+    }
+
+    public List<String> getLogChain() {
+        return logChain;
+    }
+    public int logChainLen(){
+        return logChain.size();
+    }
+    public String getLastLog(){
+        if(logChainLen() != 0)
+        {
+            return logChain.get(logChainLen()-1);
+        }
+        else
+            return null;
+    }
+
+    public String sumOflog(){
+        StringBuilder sb = new StringBuilder();
+        for(String str:logChain){
+            sb.append(str+"\n");
+        }
+//        System.out.println(sb.toString());
+        return sb.toString();
     }
 }
